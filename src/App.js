dbad8c3a169ad8e4ca0eea90f3534ec76f0bf9e7
Ata Ahmad <ataahmad@Atas-MBP.lan>
2022-06-24 00:32:25 -0700

    divided page structure into two

--- src/App.js
+++ src/App.js
@@ -2,36 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 
 function App() {
   return (
     <Router>
-    <Navbar />
-    <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<About/>} />
-    </Routes>
+      <div className="App">
+        <div className="row">
+          <div className="column left">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+          <div className="column right">
+            <Navbar />
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
