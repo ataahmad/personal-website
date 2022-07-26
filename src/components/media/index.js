eaf00231e4ccdd274c625fc938fd8a3a92f22543
Ata Ahmad <ataahmad@Atas-MacBook-Pro.local>
2022-07-25 18:01:03 -0700

    added in carousel functionality

--- src/components/media/index.js
+++ src/components/media/index.js
@@ -1,17 +1,55 @@
 import React from "react";
-import './../../App.css';
-import myImage from "./../../photos/car-personal.JPG";
+import "./#root/App.css";
+
+import image1 from "#photos/car-personal.JPG";
+import image2 from "#photos/court-personal.JPG";
+import image3 from "#photos/littleItaly-personal.JPG";
+import image4 from "#photos/moon-personal.JPG";
+import image5 from "#photos/sunset-exp-personal.JPG";
+
+
+const images = [
+    {
+        caption: "Chasing Sunsets",
+        imgPath: image1
+    },
+    {
+        caption: "Net",
+        imgPath: image2
+    },
+    {
+        caption: "That's Amore",
+        imgPath: image3
+    },
+    {
+        caption: "And It Went Like",
+        imgPath: image4
+    },
+    {
+        caption: "Death Star",
+        imgPath: image5
+    },
+];
+
 
 const Media = () => {
 
-
+    const [index, setImg] = React.useState(0);
     return (
 
         <div className="media-block">
-            <figure className="figure">
-                <img src={myImage} alt="Image" className="image"></img>
-                {/* <figcaption>For now just a cool picture, but soon to be a sick photo gallery.</figcaption> */}
-            </figure>
+            <div>
+                <figure className="figure">
+                    <img src={images[index].imgPath} alt="Image" className="image"></img>
+                    <figcaption>{images[index].caption}</figcaption>
+                </figure>
+                <button
+                    className="toggle"
+                    onClick={() => {
+                        setImg((index + 1) % images.length);
+                    }}
+                >Check out my shots.</button>
+            </div>
         </div>
 
 
