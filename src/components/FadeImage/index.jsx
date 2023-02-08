52804cddeb4c7e3d60e13a9a9fddda37dc9ace5a
Ata Ahmad <ataahmad@Atas-MBP.lan>
2023-02-08 13:20:37 -0800

    Updates thumbnails to also utilize FadeImage component

--- src/components/FadeImage/index.jsx
+++ src/components/FadeImage/index.jsx
@@ -1,13 +1,15 @@
-import { React, useState } from 'react';
+import { React, useCallback, useState } from 'react';
 
-const FadeImage = ({ src, alt, imgClass }) => {
+const FadeImage = ({ src, alt, imgClass, useClick }) => {
   const [loaded, setLoaded] = useState(false);
+  const memoizedHandleClick = useCallback(() => {}, []);
 
   return (
     <img
       src={src}
       alt={alt}
       onLoad={() => setLoaded(true)}
+      onClick={useClick}
       style={{
         opacity: loaded ? 1 : 0,
         transition: 'opacity 0.5s ease-in-out'
