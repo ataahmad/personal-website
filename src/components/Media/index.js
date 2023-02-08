096512c43d7bf9b29a6783cb76a9bc73b30a412d
Ata Ahmad <ataahmad@Atas-MBP.lan>
2023-02-08 12:06:40 -0800

    Implements image fade in effect

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import "./media.css";
 import { IMAGES } from "constants";
+import FadeImage from "components/FadeImage";
 
 
 const Media = props => {
@@ -29,11 +30,11 @@ const Media = props => {
             <h3 className="row caption">Here is a peek into my life outside of coding</h3>
             <div className="row">
                 <figure className="figure">
-                    <img 
-                    src={IMAGES[index].imgPath}
-                    alt="Email me for bookings!"
-                    className="main-image"
-                    ></img>
+                    <FadeImage
+                        src={IMAGES[index].imgPath}
+                        alt="Email me for bookings!"
+                        imgClass="main-image"
+                    ></FadeImage>
                 </figure>
             </div>
             <div className="row">
@@ -44,11 +45,11 @@ const Media = props => {
                     <button className="toggle" onClick={handleLeftClick}>
                         <span>&lt;</span>
                     </button>
-                    <img src={IMAGES[0].imgPath} onClick={handleImageClick(0)} alt="Email me for bookings!" className={`thumbnail${(index === 0 ? " selected" : "")}`}></img>
-                    <img src={IMAGES[1].imgPath} onClick={handleImageClick(1)} alt="Email me for bookings!" className={`thumbnail${(index === 1 ? " selected" : "")}`}></img>
-                    <img src={IMAGES[2].imgPath} onClick={handleImageClick(2)} alt="Email me for bookings!" className={`thumbnail${(index === 2 ? " selected" : "")}`}></img>
-                    <img src={IMAGES[3].imgPath} onClick={handleImageClick(3)} alt="Email me for bookings!" className={`thumbnail${(index === 3 ? " selected" : "")}`}></img>
-                    <img src={IMAGES[4].imgPath} onClick={handleImageClick(4)} alt="Email me for bookings!" className={`thumbnail${(index === 4 ? " selected" : "")}`}></img>
+                    <FadeImage src={IMAGES[0].imgPath} onClick={handleImageClick(0)} alt="Email me for bookings!" imgClass={`thumbnail${(index === 0 ? " selected" : "")}`} />
+                    <FadeImage src={IMAGES[1].imgPath} onClick={handleImageClick(1)} alt="Email me for bookings!" imgClass={`thumbnail${(index === 1 ? " selected" : "")}`} />
+                    <FadeImage src={IMAGES[2].imgPath} onClick={handleImageClick(2)} alt="Email me for bookings!" imgClass={`thumbnail${(index === 2 ? " selected" : "")}`} />
+                    <FadeImage src={IMAGES[3].imgPath} onClick={handleImageClick(3)} alt="Email me for bookings!" imgClass={`thumbnail${(index === 3 ? " selected" : "")}`} />
+                    <FadeImage src={IMAGES[4].imgPath} onClick={handleImageClick(4)} alt="Email me for bookings!" imgClass={`thumbnail${(index === 4 ? " selected" : "")}`} />
                     <button className="toggle" onClick={handleRightClick}>
                         <span className="arrow">&gt;</span>
                     </button>
